0ec0714b08d85dbda197fbb8d6e31269f63eeebd
Alex <my.Gamer@email.com>
2022-11-24 12:19:13 +0100

    enemy scenario spawner

--- public/game.js
+++ public/game.js
@@ -93,12 +93,37 @@ class Ball extends Phaser.Physics.Arcade.Sprite {
     this.setPosition(x, y);
     this.setFriction(10, 10);
     this.setVelocity(190, -180); // AHUYENY Diagonal movenement VECTOR
+    this.closeBounds();
+  }
+
+  sendOutBalls() {
+    this.body.setBoundsRectangle(
+      new Phaser.Geom.Triangle(500, 0, 760, 45, 600, 600)
+    );
+    this.setDepth(-1);
+    this.setVelocity(-220, 200);
+  }
+
+  closeBounds() {
     this.body.setBoundsRectangle(
       new Phaser.Geom.Rectangle(0, 0, config.width - 150, config.height - 151)
     );
+  }
 
-    // this.setVelocity(bv.x, bv.y);
-    // bv = null;
+  spawnInBox(bounds, x, y) {
+    this.body.setBoundsRectangle(bounds);
+    this.enableBody(true, true);
+    this.setAlpha(1);
+    this.setActive(true);
+    this.body.reset(x, y);
+    this.setVisible(true);
+    this.setCircle(30);
+    this.setCollideWorldBounds(true);
+    this.setBounce(1);
+    this.setPosition(x, y);
+    this.setVelocity(90, -100);
+    this.setDepth(1);
+    // this.closeBounds()
   }
 
   hit() {
@@ -128,35 +153,40 @@ class Balls extends Phaser.Physics.Arcade.Group {
   constructor(scene) {
     super(scene.physics.world, scene);
 
-    this.customBounds = new Phaser.Geom.Rectangle(0, 0, 576, 240);
     this.createMultiple({
       frameQuantity: 20,
       key: "ball",
       active: false,
       visible: false,
       classType: Ball,
-      customBoundsRectangle: this.customBounds,
-      collideWorldBounds: true,
     });
+  }
+
+  spawnBox(bounds, x, y) {
+    let ball = this.getFirstDead(false);
+    if (ball) {
+      ball.spawnInBox(bounds, x, y);
+    }
   }
 
   spawnBall(x, y) {
     let ball = this.getFirstDead(false);
     if (ball) {
       ball.spawn(x, y);
-      console.log("BALL TEST");
     }
   }
 }
 
-class Game_Scene extends Phaser.Scene {
+class GameScene extends Phaser.Scene {
   constructor() {
     super();
     this.balls;
     this.bullets;
     this.player;
     this.hitBox;
-    this.hitEnemy;
+    this.stage;
+    this.scoreText;
+    this.score
   }
 
   preload() {
@@ -170,11 +200,18 @@ class Game_Scene extends Phaser.Scene {
     var missW = 200;
     var missH = 150;
     var score = 0;
+    this.score;
+    this.stage = 1;
+    this.scoreText = this.add.text(350, 500, "Score:>"+ score);
+    this.scoreText.setColor(0xffffff);
+    this.scoreText.setDepth(10)
 
     // this.enemy.hit = new Ball(this);
     this.bullets = new Bullets(this);
     this.balls = new Balls(this);
     this.player = this.physics.add.image(300, 200, "player");
+
+    //Goal Box Setup
     this.hitBox = this.add.rectangle(
       config.width / 2 - 75,
       config.height - missH / 2,
@@ -197,13 +234,13 @@ class Game_Scene extends Phaser.Scene {
       0xa3b899
     );
     this.sideHUD = this.add.rectangle(725, 300, 150, 600, 0xae99ff);
+    var spawnBox1 = this.add.rectangle(725, 75, 80, 80, 0x111211);
+    var spawnBox4 = this.add.rectangle(725, 190, 80, 80, 0x8060ff);
+    var spawnBox2 = this.add.rectangle(725, 305, 80, 80, 0x8060ff);
 
-    // this.hitBox.setStrokeStyle(4, 0xefc53f);
     this.bullets.children;
     this.balls.children;
-    console.log("spawn test >", this.balls);
 
-    // this.balls.spawnBall(300, 200, 4);
     this.balls.x = 300;
     this.balls.y = 300;
     Phaser.Actions.SetXY(this.balls.getChildren(), -100, -200, 32);
@@ -226,10 +263,9 @@ class Game_Scene extends Phaser.Scene {
     // this.hitBox.setGravity(0, (config.physics.arcade.gravity.y) * -1);
 
     //Ball Setup
-    this.balls.spawnBall(300, 200);
-    this.balls.spawnBall(280, 180);
-    this.balls.spawnBall(260, 160);
-    this.balls.spawnBall(240, 140);
+    // this.balls.spawnBall(280, 180);
+    // this.balls.spawnBall(260, 160);
+    // this.balls.spawnBall(240, 140);
     // this.balls.spawnBall(300, 200, 4);
 
     //Player Setup - Move to Player Class
@@ -238,6 +274,9 @@ class Game_Scene extends Phaser.Scene {
     this.player.setPushable(false);
     this.player.setCollideWorldBounds(true);
     this.player.setGravity(0, config.physics.arcade.gravity.y * -1);
+    this.player.body.setBoundsRectangle(
+      new Phaser.Geom.Rectangle(0, 0, config.width - 150, config.height - 150)
+    );
 
     //Input Setup
     this.inputKeys = this.input.keyboard.addKeys({
@@ -279,12 +318,28 @@ class Game_Scene extends Phaser.Scene {
       }
     );
 
+    this.physics.add.collider(
+      this.balls,
+      this.player,
+      function (ball, player) {
+        
+        player
+
+        ball.body.checkCollision = true;
+        tween.restart();
+        tween.data[0].target = player;
+        tween.play();
+      }
+    );
+
     //Add on Overlap Event to game class
     this.physics.add.overlap(this.balls, this.hitBox, function (hitBox, ball) {
       ball.disableBody(true, true);
       ball.setActive(false);
       if (ball.isHit) {
         score++;
+        this.score = score
+        console.log('this>' + this.score);
         console.log(score);
         tween.pause();
         tween.restart();
@@ -300,8 +355,6 @@ class Game_Scene extends Phaser.Scene {
         ball.setActive(false);
 
         if (ball.isHit) {
-          score++;
-          console.log(score);
           tween.pause();
           tween.restart();
         }
@@ -317,21 +370,84 @@ class Game_Scene extends Phaser.Scene {
         ball.setActive(false);
 
         if (ball.isHit) {
-          tween.pause();
-          score++;
           console.log(score);
           tween.restart();
         }
       }
     );
+
+    //Scenario game Start
+    // timedEvent = this.time.delayedCall(3000, onEvent, [], this);
+
+    if ((this.stage == 1)) {
+      var borderBox1 = new Phaser.Geom.Rectangle(680, 30, 80, 80);
+      this.balls.spawnBox(borderBox1, 750, 32);
+      this.balls.spawnBox(borderBox1, 730, 44);
+      this.balls.spawnBox(borderBox1, 720, 60);
+      this.balls.spawnBox(borderBox1, 690, 75);
+
+      this.time.delayedCall(
+        3000,
+        function () {
+          this.balls.getChildren().forEach((ball) => {
+            ball.sendOutBalls();
+            ball.setVelocity(
+              Math.floor(Math.random() * (200 - 30 + 1)) + 30,
+              Math.floor(Math.random() * (200 - 60 + 1)) + 60
+            );
+          });
+          this.time.delayedCall(
+            900,
+            function () {
+              this.balls.getChildren().forEach((ball) => {
+                ball.closeBounds();
+              });
+            },
+            [],
+            this
+          );
+        },
+        [],
+        this
+      );
+    }
+    if (this.stage == 2 && this.stage > 1) {
+      var borderBox2 = new Phaser.Geom.Rectangle(680, 145, 80, 80);
+      this.balls.spawnBox(borderBox2, 750, 110);
+      this.balls.spawnBox(borderBox2, 730, 160);
+      this.balls.spawnBox(borderBox2, 720, 180);
+      this.balls.spawnBox(borderBox2, 690, 140);
+
+      this.time.delayedCall(
+        3000,
+        function () {
+          this.balls.getChildren().forEach((ball) => {
+            ball.sendOutBalls();
+            ball.setVelocity(
+              Math.floor(Math.random() * (200 - 30 + 1)) + 30,
+              Math.floor(Math.random() * (200 - 60 + 1)) + 60
+            );
+          });
+          this.time.delayedCall(
+            900,
+            function () {
+              this.balls.getChildren().forEach((ball) => {
+                ball.closeBounds();
+              });
+            },
+            [],
+            this
+          );
+        },
+        [],
+        this
+      );
+    }
   }
 
   update() {
-    //[FOR TESTING] Gib more if runout
-    this.balls.countActive(true) < 4 ? this.balls.spawnBall(280, 180) : 0x0;
-    this.balls.countActive(true) < 4 ? this.balls.spawnBall(280, 180) : 0x0;
-    this.balls.countActive(true) < 4 ? this.balls.spawnBall(280, 180) : 0x0;
-    this.balls.countActive(true) < 4 ? this.balls.spawnBall(280, 180) : 0x0;
+    (this.balls.countActive(true) < 1 && this.stage !=2) ? this.stage++ : 0x0;
+    // console.log(this.stage);
 
     //Player onUpdate() SETUP
     //Player Movement and input
@@ -377,7 +493,7 @@ const config = {
       gravity: { y: 200 },
     },
   },
-  scene: Game_Scene,
+  scene: GameScene,
 };
 
 let game = new Phaser.Game(config);
